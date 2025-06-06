2e8fed6de70962794b8dfe933bbe11333a3425a6
Icaro <5kandradee@gmail.com>
2025-06-06 09:38:41 -0300

    feat: reasons component implemented

---
+++ src/app/components/reasons/reasons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reasons',
+  standalone: false,
+  templateUrl: './reasons.component.html',
+  styleUrl: './reasons.component.scss'
+})
+export class ReasonsComponent {
+
+}
